7771531fc3e602b09419caedfad13a8a5207d84f
janaite <janaite@gmail.com>
2021-06-23 18:19:57 -0300

    Initial commit

---
+++ backend/src/main/java/dev/janaite/dslearnbds/DslearnbdsApplication.java
@@ -0,0 +1,12 @@
+package dev.janaite.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsApplication.class, args);
+	}
+}
