0ea13f5accaab9be5b77ae79206855a84d44e489
janaite <janaite@gmail.com>
2021-06-24 14:20:18 -0300

    Resource entity

---
+++ backend/src/main/java/dev/janaite/dslearnbds/entities/enums/ResourceType.java
@@ -0,0 +1,6 @@
+package dev.janaite.dslearnbds.entities.enums;
+
+public enum ResourceType {
+
+	LESSONS_ONLY, LESSON_TASK, FORUM, EXTERNAL_LINK;
+}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'im
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z',1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z',1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'trilha principal do curso', 1, 'https://jjn.ninja/wp-content/uploads/2019/11/espresso.jpg', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://jjn.ninja/wp-content/uploads/2019/11/espresso.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://jjn.ninja/wp-content/uploads/2019/11/espresso.jpg', 0, 1);
