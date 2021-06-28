12acfc9b41d605f89f0b2d6d68771d29796a7457
janaite <janaite@gmail.com>
2021-06-28 19:54:42 -0300

    Topic entity

--- backend/src/main/java/dev/janaite/dslearnbds/entities/Offer.java
+++ backend/src/main/java/dev/janaite/dslearnbds/entities/Offer.java
@@ -24,19 +24,22 @@ public class Offer implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String edition;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant startMoment;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant endMoment;
 
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
-	
+
 	@OneToMany(mappedBy = "offer")
 	private List<Resource> resouces = new ArrayList<>();
+
+	@OneToMany(mappedBy = "offer")
+	private List<Topic> topics = new ArrayList<>();
 
 	public Offer() {
 	}
@@ -83,6 +86,10 @@ public class Offer implements Serializable {
 
 	public List<Resource> getResouces() {
 		return resouces;
+	}
+
+	public List<Topic> getTopics() {
+		return topics;
 	}
 
 	@Override
