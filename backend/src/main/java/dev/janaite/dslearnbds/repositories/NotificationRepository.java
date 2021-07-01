af2ad067a33b36573bbd4b2ec0b2dcc92503add6
janaite <janaite@gmail.com>
2021-07-01 18:28:23 -0300

    Custom notifications for current user

--- backend/src/main/java/dev/janaite/dslearnbds/repositories/NotificationRepository.java
+++ backend/src/main/java/dev/janaite/dslearnbds/repositories/NotificationRepository.java
@@ -1,9 +1,14 @@
 package dev.janaite.dslearnbds.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.janaite.dslearnbds.entities.Notification;
+import dev.janaite.dslearnbds.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
+	
+	Page<Notification> findByUser(User user, Pageable pageable);
 
 }
---
+++ backend/src/main/java/dev/janaite/dslearnbds/services/NotificationService.java
@@ -0,0 +1,30 @@
+package dev.janaite.dslearnbds.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import dev.janaite.dslearnbds.dto.NotificationDTO;
+import dev.janaite.dslearnbds.entities.Notification;
+import dev.janaite.dslearnbds.entities.User;
+import dev.janaite.dslearnbds.repositories.NotificationRepository;
+
+@Service
+public class NotificationService {
+	
+	@Autowired
+	private NotificationRepository repository;
+	
+	@Autowired
+	private AuthService authService;
+	
+	@Transactional(readOnly = true)
+	public Page<NotificationDTO> notificationsForCurrentUser(Pageable pageable) {
+		User user = authService.getCurrentUserAuthenticated();
+		Page<Notification> page = repository.findByUser(user, pageable);
+		return page.map(p -> new NotificationDTO(p));
+	}
+
+}
