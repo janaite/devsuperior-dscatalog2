56244aaba2d8912c45b36d8e62280e390543d6d4
janaite <janaite@gmail.com>
2021-07-01 22:43:39 -0300

    enable Refresh token

--- backend/src/main/java/dev/janaite/dslearnbds/config/AuthorizatioServerConfig.java
+++ backend/src/main/java/dev/janaite/dslearnbds/config/AuthorizatioServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -27,7 +28,7 @@ public class AuthorizatioServerConfig extends AuthorizationServerConfigurerAdapt
 
 	@Value("${security.oauth2.client.client-secret}")
 	private String clientSecret;
-	
+
 	@Value("${jwt.duration}")
 	private Integer jwtDuration;
 
@@ -42,9 +43,12 @@ public class AuthorizatioServerConfig extends AuthorizationServerConfigurerAdapt
 
 	@Autowired
 	private AuthenticationManager authenticationManger;
-	
+
 	@Autowired
 	private JwtTokenEnhancer tokenEnhancer;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -53,24 +57,26 @@ public class AuthorizatioServerConfig extends AuthorizationServerConfigurerAdapt
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory()
-			.withClient(clientId)
-			.secret(passwordEncoder.encode(clientSecret))
-			.scopes("read", "write")
-			.authorizedGrantTypes("password")
-			.accessTokenValiditySeconds(jwtDuration);
+		clients	.inMemory()
+				.withClient(clientId)
+				.secret(passwordEncoder.encode(clientSecret))
+				.scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(jwtDuration)
+				.refreshTokenValiditySeconds(jwtDuration);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		
+
 		TokenEnhancerChain chain = new TokenEnhancerChain();
 		chain.setTokenEnhancers(List.of(accessTokenConverter, tokenEnhancer));
-		
-		endpoints.authenticationManager(authenticationManger)
-			.tokenStore(tokenStore)
-			.accessTokenConverter(accessTokenConverter)
-			.tokenEnhancer(chain);
+
+		endpoints	.authenticationManager(authenticationManger)
+					.tokenStore(tokenStore)
+					.accessTokenConverter(accessTokenConverter)
+					.tokenEnhancer(chain)
+					.userDetailsService(userDetailsService);
 	}
 
 }
